85010c1ea25dcb2a0c4e3460fc743c884991ed66
Jahanvi <jahanvimp128@gmail.com>
2023-08-07 00:09:41 +0530

    server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json())
+
+const PORT = 3000
+app.listen(PORT,() => {
+    console.log("Server is running on port ",PORT);
+})
